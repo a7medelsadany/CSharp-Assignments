65ad7d84c864c292004c2b0feb8babe2fb57d734
ahmed abd alaziz ali abd alaziz elsadany <108400733+a7medelsadany@users.noreply.github.com>
2025-07-12 19:05:15 +0300

    Add Assignment 05

---
+++ Assignment05-C-Sharp/Program.cs
@@ -0,0 +1,168 @@
+﻿namespace Assignment05_C_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ass5
+            #region Q1.enter number and print from 1 to this number
+            //Console.WriteLine("please enter number");
+            //int.TryParse(Console.ReadLine(), out int num);
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+            #region Q2.enter number and calcuate multiplication 
+            //Console.WriteLine("please enter number");
+            //int.TryParse(Console.ReadLine(), out int number);
+            //int result;
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    result = i * number;
+            //    Console.WriteLine(result);
+            //}
+            #endregion
+            #region Q3.print even number
+            //Console.WriteLine("please enter number");
+            //int.TryParse(Console.ReadLine(), out int n);
+            //for(int i = 1; i < n; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+            #region Q6.Reverse string
+            //Console.WriteLine("please enter string ");
+            //string input = Console.ReadLine();
+
+            //string[] words = input.Split(' ');
+            //Array.Reverse(words);
+
+            //string reversed=string.Join(" ", words);
+            //Console.WriteLine(reversed);
+            #endregion
+            #region Q7.reverse integer
+            //Console.WriteLine("Enter a number:");
+            //string num = Console.ReadLine();
+            //char[] arr = num.ToCharArray();
+            //Array.Reverse(arr);
+            //Console.WriteLine("Reversed number: " + new string(arr));
+            #endregion
+            #region َQ8.prime numbers between two numbers
+            //Console.WriteLine("Enter start number:");
+            //int start = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter end number:");
+            //int end = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Prime numbers between {start} and {end} are:");
+
+            //for (int num = start; num <= end; num++)
+            //{
+            //    if (num > 1) 
+            //    {
+            //        bool isPrime = true;
+
+            //        for (int i = 2; i < num; i++)
+            //        {
+            //            if (num % i == 0)
+            //            {
+            //                isPrime = false;
+            //                break;
+            //            }
+            //        }
+
+            //        if (isPrime)
+            //            Console.Write(num + " ");
+            //    }
+            //}
+
+
+            #endregion
+            #region Q9.from decimal to binary
+            //Console.WriteLine("Enter a decimal number:");
+            //int.TryParse(Console.ReadLine(), out int dec);
+            //string binary = Convert.ToString(dec, 2);
+            //Console.WriteLine("Binary representation: " + binary);
+            #endregion
+            #region Q11.matrix
+            //Console.WriteLine("please enter number");
+            //int.TryParse(Console.ReadLine(), out int numberMatrix);
+            //for(int i=1;i<=numberMatrix;i++)
+            //{
+            //    for(int j = 1; j <= numberMatrix; j++)
+            //    {
+            //        if (i == j)
+            //        {
+            //            Console.Write(" 1 ");
+            //        }
+            //        else
+            //        {
+            //            Console.Write(" 0 ");
+            //        }
+            //    }
+            //    Console.WriteLine(" ");
+            //}
+            #endregion
+            #region Q12.sum element of array
+            //int[] arr = [1, 2, 3, 4, 5];
+            //int sumOfArray=arr.Sum();
+            //Console.WriteLine(sumOfArray);
+            #endregion
+            #region Q13.sort array
+            //int[] arrSort = [3, 4, 6, 8, 0, 5, 2];
+            //Array.Sort(arrSort);
+            //for(int i = 0; i <= arrSort.Length; i++)
+            //{
+            //    Console.WriteLine(arrSort[i]);
+            //}
+            #endregion
+            #region Q14.count of array
+            //int[] a = [1, 2, 3, 4, 5];
+            //int countOfArray = a.Count();
+            //Console.WriteLine($"the count of array : {countOfArray}");
+            #endregion
+            #region Q15.max and min of array
+            //int[] numb = [1, 3, 4, 6, 8];
+            //int maximum = numb.Max();
+            //int minimum = numb.Min();
+            //Console.WriteLine($"the max number is {maximum}");
+            //Console.WriteLine($"the min number is {minimum}");
+            #endregion
+            #region Q16.find second largest element in array
+            //int[] arr = { 5, 9, 2, 8, 7 };
+            //Array.Sort(arr);
+            //Console.WriteLine($"Second largest element is:{arr[arr.Length - 2]} ");
+            #endregion
+            #region Q17.
+
+            #endregion
+            #region Q18.reverse words
+            //Console.WriteLine("please enter words");
+            //string name = Console.ReadLine();
+            //string[] names = name.Split(' ');
+            //Array.Reverse(names);
+            //string reversed=string.Join(" ", names);
+            //Console.WriteLine(reversed);
+            #endregion
+            #region Q19.
+            #endregion
+            #region Q20.reverse one D array
+            //int[] arr = new int[5];
+            //Console.WriteLine("please enter 5 numbers ");
+            //for(int i =0; i < arr.Length; i++)
+            //{
+            //    int.TryParse(Console.ReadLine(), out arr[i]);
+            //}
+            //for(int i = arr.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(arr[i]+" ");
+            //}
+            #endregion
+            #endregion
+        }
+    }
+}
